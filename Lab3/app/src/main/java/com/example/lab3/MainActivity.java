6c412d793e3bb1a8c6e0b8d4d4ea5080d21d677c
jenny <jeand987@student.liu.se>
2024-06-13 14:10:09 +0200

    labb3 början

---
+++ Lab3/app/src/main/java/com/example/lab3/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.lab3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
